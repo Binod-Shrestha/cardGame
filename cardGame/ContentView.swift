6f7b741fe501816fd00bca13a4429346f4670ae9
Binod Shrestha <shresthabinod.ca@gmail.com>
2020-09-12 17:49:52 -0400

    Initial Commit

---
+++ cardGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cardGame
+//
+//  Created by Binod Shrestha on 2020-09-12.
+//  Copyright © 2020 Binod. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
